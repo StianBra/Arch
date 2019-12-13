3ac41b2870af3e033314924124ca3295bdeed4f3
Stian Bratlie <stiabra@stud.ntnu.no>
2019-12-13 13:30:42 +0100

    Updated script for installing fonts, still not quite completed with regards to CLI arguments

--- scripts/installFonts.sh
+++ scripts/installFonts.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
+# Created by:	Stian Bratlie
+# Contact:	stiabra@stud.ntnu.no
+# Last updated:	13.12.2019
+# Used for installing fonts (files with type .ttf and .otf) from a given folder, 
+# to another folder (typically the system-folder for fonts).
+# TODO #1: Parse command-line arguments properly
+# TODO #2: Correct directory-checking on line 23
+
+# Username of the current user
+username=$(whoami)
+
+# Gets the source folder of the fonts (one folder above the script, and then down into ./fonts)
+sourceDir="$(dirname $(realpath $(dirname ${BASH_SOURCE[0]})))""/fonts"
+
+# Gets the destination folder for the fonts
+targetDir="/usr/share/fonts/$username"
+
 echo "Installing fonts..."
-sudo mkdir /usr/share/fonts/RD
 
-echo "Moving fonts to '/usr/share/fonts/RD'..."
-sudo cp ../fonts/*.ttf ../fonts/*.otf /usr/share/fonts/RD
+# Creates targetDir if it does not already exist
+if [[ ! -d "$targetDir" ]]
+then
+	echo "Creating '$targetDir'..."
+	sudo mkdir "$targetDir"
+fi
 
+# Moves (installs) the fonts
+echo "Moving fonts from '$sourceDir' to '$targetDir'..."
+sudo cp $sourceDir/*.ttf $sourceDir/*.otf $targetDir
+
+# Apply new fonts
+fc-cache -f -v
+
+# We're done!
+echo "Done installing fonts!"
