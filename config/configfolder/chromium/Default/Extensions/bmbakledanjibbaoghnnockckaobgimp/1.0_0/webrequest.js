71a46e0625c1e55d4357fb36494c59e17ddf0601
Stian Bratlie <stiabra@stud.ntnu.no>
2020-06-21 01:04:29 +0200

    Fixed pw leak from chromium

--- config/configfolder/chromium/Default/Extensions/bmbakledanjibbaoghnnockckaobgimp/1.0_0/webrequest.js
+++
@@ -1,7 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    return {redirectUrl: details.url.substring(43)};
-  },
-  {urls: ["*://steamcommunity.com/linkfilter/*"]},
-  ["blocking"]
-);
--- config/configfolder/chromium/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd/4.34.0.6_1/environment.js
+++
@@ -1,2 +0,0 @@
-var Environment={version:"4.34.0.6",environment:"prod",lastpass_url:"https://lastpass.com/"};
-//# sourceMappingURL=sourcemaps/environment.js.map
